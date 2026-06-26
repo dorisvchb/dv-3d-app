f1243c8edad46902a9b6729833cb413eaa538ee1
dorisvchb <100151176+dorisvchb@users.noreply.github.com>
2026-06-26 13:17:21 -0500

    Primer cambio en archivo App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>dv3D, la app que permite visualizar objetos en 3D de forma interactiva  </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +16,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingLeft:20,
   },
 });

05c5a8d66b99abc6cbfc7a59d05b4070079aeebf
dorisvchb <100151176+dorisvchb@users.noreply.github.com>
2026-07-24 14:38:30 -0500

    feat: integrar AuthProvider y RootNavigator en App.js

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { AuthProvider } from './context/AuthContext';
+import RootNavigator from './navigation/RootNavigator';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>dv3D, la app que permite visualizar objetos en 3D de forma interactiva  </Text>
-      <StatusBar style="auto" />
-    </View>
+    <AuthProvider>
+      <RootNavigator />
+    </AuthProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingLeft:20,
-  },
-});
+

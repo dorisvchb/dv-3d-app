f364b23bf762e62df919ffb481a6ad898c54a871
dorisvchb <100151176+dorisvchb@users.noreply.github.com>
2026-07-29 17:08:25 -0500

    feat(viewer): integrar polyfills y GestureHandlerRootView en App.js

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
+// App.js
+import './polyfills';
+import 'react-native-gesture-handler';
+
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { AuthProvider } from './context/AuthContext';
 import RootNavigator from './navigation/RootNavigator';
 
 export default function App() {
   return (
-    <AuthProvider>
-      <RootNavigator />
-    </AuthProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <AuthProvider>
+        <RootNavigator />
+      </AuthProvider>
+    </GestureHandlerRootView>
   );
 }
-
-
